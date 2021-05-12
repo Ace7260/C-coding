42b9161fda8ef351ae57a2fe59e5318cd59c5463
Irakoze Eddy Nobel <83595479+Ace7260@users.noreply.github.com>
2021-05-12 11:33:07 -0700

    Update challenge.h

---
+++ challenge.h
@@ -0,0 +1,18 @@
+#ifndef
+int SaisirEntier();
+int SaisirNote();
+void SaisirNnote(int t[],int u);
+void Menu();
+void Arreter();
+void Afficher(int t[],int u);
+void Remplir(int t[],int u);
+int Occurence(int t[],int Taille,int v);
+bool Existence(int t[],int Taille,int ex);
+void Occurences(int t[],int Taille);
+int PetiteNote(int t[],int Taille);
+int GrandeNote(int t[],int u);
+int somme(int t[],int u);
+int moyenne(int t[],int Taille);
+void Trier(int t[],int u);
+int Search();
+#endif
